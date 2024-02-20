3c2349ebd3832b5269baa34f1ec3297f5ae1917a
Pedro Gabriel <bielpedro300@gmail.com>
2024-02-20 14:23:01 -0300

    First commit

---
+++ src/app/components/quiz/quiz.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quizz_questions.json'
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent implements OnInit{
+  title:string = "";
+  questions:any;
+  questionSelected: any;
+
+  answers:string[] = [];
+  answerSelect:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  ngOnInit(): void {
+    if (quiz_questions) {
+      this.finished = false;
+      this.title = quiz_questions.title;
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoose(value: string): void {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer:string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelect = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results];
+    }
+  }
+
+  async checkResult(answers:string[]) {
+    const result = answers.reduce((previous, current, index, arr) => {
+      if (arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
+}
